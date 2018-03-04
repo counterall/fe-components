0028605ad509b24ab007a50f2d0fcac54c0f0710
Kan Cong <kcongmj23@gmail.com>
2018-03-05 01:21:49 +0200

    vue: toggle dropdown and instruction

--- dest/js/inventory.js
+++ dest/js/inventory.js
@@ -1,11 +1,32 @@
 jQuery(function($){
 
+    var inventoryApp = new Vue({
+        el: "#inventory-app",
+        data: {
+           showDropdown: false,
+           cityList:['No available city'],
+           cityChosen: null
+        },
+        methods: {
+            toggleDropdown: function(){
+                this.showDropdown = !this.showDropdown;
+            },
+            switchList: function () {
+            }
+        },
+        mounted: function() {
+            
+        },
+        created: function() {
+ 
+        }
+    });
+
+    /* Render Select2 dropdown*/
     $('.city-selector').select2({
         placeholder: 'Valitse kaupunki',
         width: "element",
         theme: "marimekko"
-    }); 
-
+    });
     
-
 });
