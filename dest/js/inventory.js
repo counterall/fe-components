7405e9357be322ab2009e805da865a36e2246839
Kan Cong <kcongmj23@gmail.com>
2018-03-05 03:28:48 +0200

    select2 dropdown fix wip

--- dest/js/inventory.js
+++ dest/js/inventory.js
@@ -1,32 +1,69 @@
-jQuery(function($){
-
-    var inventoryApp = new Vue({
+jQuery(function ($) {
+       
+    window.inventoryApp = new Vue({
         el: "#inventory-app",
         data: {
-           showDropdown: false,
-           cityList:['No available city'],
-           cityChosen: null
+            dropDownClass: "city-selector",
+            showDropdown: false,
+            cityList: [
+                { id: 1, text: 'Hello' },
+                { id: 2, text: 'World' }
+            ],
+            cityChosen: ''
         },
         methods: {
-            toggleDropdown: function(){
+            toggleDropdown: function () {
                 this.showDropdown = !this.showDropdown;
             },
             switchList: function () {
+                
             }
         },
-        mounted: function() {
-            
-        },
-        created: function() {
- 
-        }
-    });
+        mounted: function () {
 
-    /* Render Select2 dropdown*/
-    $('.city-selector').select2({
-        placeholder: 'Valitse kaupunki',
-        width: "element",
-        theme: "marimekko"
-    });
-    
+        },
+        created: function () {
+
+        },
+        components: {
+            'select2': {
+                props: ['options', 'city'],
+                template: "#select2-template",
+                mounted: function () {
+                    var vm = this
+                    console.log(this);
+                    $(this.$el)
+                        // init select2
+                        .select2({
+                            data: this.options,
+                            placeholder: 'Valitse kaupunki',
+                            width: "element",
+                            theme: "marimekko"
+                        })
+                        .val(this.city)
+                        .trigger('change')
+                        // emit event on change.
+                        .on('change', function () {
+                            vm.$emit('input', this.city)
+                        })
+                },
+                watch: {
+                    value: function (city) {
+                        // update value
+                        $(this.$el).val(city)
+                        console.log($(this.$el).val(city));
+                    },
+                    options: function (cityList) {
+                        // update options
+                        $(this.$el).empty().select2({
+                            data: cityList
+                        })
+                    }
+                },
+                destroyed: function () {
+                    $(this.$el).off().select2('destroy')
+                }
+            }
+        }
+    })
 });
