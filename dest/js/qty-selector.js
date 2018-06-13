5dd0600b266be4a2098480133af3170c76b724ec
Kan Cong <kcongmj23@gmail.com>
2018-06-13 12:36:22 +0300

    code refactor

--- dest/js/qty-selector.js
+++ dest/js/qty-selector.js
@@ -6,11 +6,11 @@ function initQuantitySelector($selector){
     var $displayedVal = $selector.find('.qty-val');
     /* by default, max is 10 and min is 1 */
     var maxVal = $input.attr('max') ? $input.attr('max') : 10;
-    var minVal = $input.attr('min') ? $input.attr('max') : 1;
+    var minVal = $input.attr('min') ? $input.attr('min') : 1;
 
     $descrease.on('click', function() {
         var currentVal = parseInt($input.val());
-        var newVal = currentVal - 1 >= 1 ? currentVal - 1 : 1;
+        var newVal = currentVal - 1 >= minVal ? currentVal - 1 : minVal;
         if (newVal !== currentVal) {
             $input.val(newVal);
             $displayedVal.text(newVal);
@@ -19,7 +19,7 @@ function initQuantitySelector($selector){
 
     $inscrease.on('click', function () {
         var currentVal = parseInt($input.val());
-        var newVal = currentVal + 1 <= 10 ? currentVal + 1 : 10;
+        var newVal = currentVal + 1 <= maxVal ? currentVal + 1 : maxVal;
         if (newVal !== currentVal) {
             $input.val(newVal);
             $displayedVal.text(newVal);
