41b827c65666523a907adfcaed7098d8163bacf7
Kan Cong <kan.cong@marimekko.com>
2019-01-22 15:02:35 +0200

    solution one to restore scroll position manually

--- dest/js/scroll-restoration.js
+++ dest/js/scroll-restoration.js
@@ -6,7 +6,7 @@ jQuery(function($) {
     }).done(function (data) {
         var $wrapper = $('.wrapper');
         var imgNames = data['cover-name'];
-        for (var index = 0; index < 50; index++) {
+        for (var index = 0; index < 51; index++) {
            var randomID = Math.floor(Math.random() * 4);
            var imgName = "dest/img/" + imgNames[randomID] + '.jpg';
            var $link = $('<a/>', {
@@ -24,6 +24,39 @@ jQuery(function($) {
     }).fail(function () {
         
     });
-
-    
 });
+
+// FOR non-chrome browsers
+if (!/chrome/i.test(window.navigator.userAgent)) {
+    // Turn off auto page position restoration
+    ('scrollRestoration' in window.history) && (window.history.scrollRestoration = 'manual');
+
+    // scroll restoration after history entry has been accessed
+    $(window).on('popState', function () {
+        console.log('history entry accessed!', window.scrollY);
+        if (window.history.state) {
+            var xPos = window.history.state.scrollX ? window.history.state.scrollX : 0;
+            var yPos = window.history.state.scrollY ? window.history.state.scrollY : 0;
+            if (yPos) {
+                var checkHeight = setInterval(() => {
+                    console.log(window.scrollY, yPos, $(document).height());
+                    if ($(document).height() >= yPos) {
+                        window.scrollTo(xPos, yPos);
+                        clearInterval(checkHeight);
+                    }
+                }, 100);
+            }
+            
+        }
+    });
+
+    // Mark location before page unloads
+    $(window).on('beforeunload', function () {
+        history.pushState({
+            scrollY: window.scrollY,
+            scrollX: window.scrollX
+        }, 'cateogry page', "");
+    }).on('load', function() {
+        $(this).trigger('popState');
+    }).on('scroll', function(){console.log(window.scrollY);});
+}
