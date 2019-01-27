57902c8736ded3a5d8314194800941fc9cd863d2
Kan Cong <kan.cong@digia.com>
2019-01-27 15:09:28 +0200

    pause and play button will stop and continue the timer

--- dest/js/hour-reporting.js
+++ dest/js/hour-reporting.js
@@ -1,13 +1,35 @@
-    var beautifyTime = (digit) => digit < 10 ? ("0" + digit.toString()) : digit;
-    if (!sessionStorage.timebase) {
-      sessionStorage.setItem('timebase', Date.now());
-    }
+    jQuery(($)=>{
+      var beautifyTime = (digit) => digit < 10 ? ("0" + digit.toString()) : digit;
+      if (!sessionStorage.timebase) {
+        sessionStorage.setItem('timebase', Date.now());
+      }
 
-    var timeDiff = Math.floor((Date.now() - sessionStorage.timebase) / 1000);
-    setInterval(() => {
-      timeDiff++;
-      var hour = beautifyTime(Math.floor(timeDiff / 3600));
-      var min = beautifyTime(Math.floor(timeDiff % 3600 / 60));
-      var sec = beautifyTime(timeDiff % 3600 % 60);
-      $('#time').html(`${hour}:${min}:${sec}`);
-    }, 1000);
+      var showTimer = () => {
+        var timeDiff = Math.floor((Date.now() - sessionStorage.timebase) / 1000);
+        var trackTime = setInterval(() => {
+          timeDiff++;
+          var hour = beautifyTime(Math.floor(timeDiff / 3600));
+          var min = beautifyTime(Math.floor(timeDiff % 3600 / 60));
+          var sec = beautifyTime(timeDiff % 3600 % 60);
+          $('#time').html(`${hour}:${min}:${sec}`);
+        }, 1000);
+        return trackTime;
+      }
+      
+      var timer = showTimer();
+      var pauseTime = 0;
+      $('#pause').on('click', function () {
+        var newStatus = $(this).data('status') == 'play' ? "pause" : "play";
+        $(this).data('status', newStatus);
+        if ($(this).data('status') == 'play') {
+          sessionStorage.timebase = parseInt(sessionStorage.timebase) + (Date.now() - sessionStorage.pauseTS);
+          timer = showTimer();
+          $(this).text('pause');
+        }else {
+          clearTimeout(timer);
+          sessionStorage.pauseTS = Date.now();
+          $(this).text('play');
+        }
+      });
+    })
+    
